c7ceb2dcc57d7463a9bd7e71c6c3edeb295dc07a
Spencer Williams <srwilliams349@gmail.com>
2023-05-12 19:08:46 -0400

    create spring boot mvc project, add jpa project dependency

---
+++ AlpenaRestaurants/src/main/java/com/skilldistillery/jparestaurant/AlpenaRestaurantsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jparestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpenaRestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpenaRestaurantsApplication.class, args);
+	}
+
+}
---
+++ AlpenaRestaurants/src/test/java/com/skilldistillery/jparestaurant/AlpenaRestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jparestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpenaRestaurantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
